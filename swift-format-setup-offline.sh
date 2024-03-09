38885103260986a038e5e6e7b9f10d16425caeaf
david-swift <haenerdavid@icloud.com>
2024-03-09 09:32:11 +0100

    Update to Swift 5.10

--- swift-format-setup-offline.sh
+++ swift-format-setup-offline.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 mkdir -p .build/{checkouts,repositories}
 cd .build/repositories || exit 1
-mkdir -p swift-argument-parser-59ba1edd swift-syntax-cb9339b1 swift-system-b9feb0b6 swift-tools-support-core-1e273aae swift-markdown-b692ce3c swift-cmark-4b3746f9
+mkdir -p swift-argument-parser-54a11a8d swift-syntax-463e60c9 swift-markdown-6b045e7c swift-cmark-7ac81116
 cd ..
-cp -R ../{swift-argument-parser,swift-syntax,swift-system,swift-tools-support-core,swift-markdown,swift-cmark} checkouts
+cp -R ../{swift-argument-parser,swift-syntax,swift-markdown,swift-cmark} checkouts
 cd checkouts || exit 1
-cp -Rv swift-argument-parser/.git/* ../repositories/swift-argument-parser-59ba1edd/
-cp -Rv swift-syntax/.git/* ../repositories/swift-syntax-cb9339b1/
-cp -Rv swift-system/.git/* ../repositories/swift-system-b9feb0b6/
-cp -Rv swift-tools-support-core/.git/* ../repositories/swift-tools-support-core-1e273aae/
-cp -Rv swift-markdown/.git/* ../repositories/swift-markdown-b692ce3c/
-cp -Rv swift-cmark/.git/* ../repositories/swift-cmark-4b3746f9/
+cp -Rv swift-argument-parser/.git/* ../repositories/swift-argument-parser-54a11a8d/
+cp -Rv swift-syntax/.git/* ../repositories/swift-syntax-463e60c9/
+cp -Rv swift-markdown/.git/* ../repositories/swift-markdown-6b045e7c/
+cp -Rv swift-cmark/.git/* ../repositories/swift-cmark-7ac81116/
--- swift-lint-setup-offline.sh
+++ swift-lint-setup-offline.sh
@@ -2,16 +2,25 @@
 mkdir -p .build/{checkouts,repositories}
 cd .build/repositories || exit 1
 mkdir -p CollectionConcurrencyKit-9b263e6b CryptoSwift-72c2bbc7 SourceKitten-45a013ca swift-argument-parser-59ba1edd swift-syntax-cb9339b1 SwiftyTextTable-dce42391 SWXMLHash-0bda306e Yams-00fc82b0
+mkdir -p CollectionConcurrencyKit-e8d5854d CryptoSwift-02302490 SourceKitten-8ccd7df5 swift-argument-parser-54a11a8d swift-syntax-463e60c9 SwiftyTextTable-cca6ebd7 SWXMLHash-ff0954f2 Yams-fd9f2519
 cd ..
 cp -R ../{CollectionConcurrencyKit,CryptoSwift,SourceKitten,swift-argument-parser,swift-syntax,SwiftyTextTable,SWXMLHash,Yams} checkouts
 cd checkouts || exit 1
 cp -Rv CollectionConcurrencyKit/.git/* ../repositories/CollectionConcurrencyKit-9b263e6b
+cp -Rv CollectionConcurrencyKit/.git/* ../repositories/CollectionConcurrencyKit-e8d5854d
 cp -Rv CryptoSwift/.git/* ../repositories/CryptoSwift-72c2bbc7
+cp -Rv CryptoSwift/.git/* ../repositories/CryptoSwift-02302490
 cp -Rv SourceKitten/.git/* ../repositories/SourceKitten-45a013ca
+cp -Rv SourceKitten/.git/* ../repositories/SourceKitten-8ccd7df5
 cp -Rv swift-argument-parser/.git/* ../repositories/swift-argument-parser-59ba1edd
+cp -Rv swift-argument-parser/.git/* ../repositories/swift-argument-parser-54a11a8d
 cp -Rv swift-syntax/.git/* ../repositories/swift-syntax-cb9339b1
+cp -Rv swift-syntax/.git/* ../repositories/swift-syntax-463e60c9
 cp -Rv SwiftyTextTable/.git/* ../repositories/SwiftyTextTable-dce42391
+cp -Rv SwiftyTextTable/.git/* ../repositories/SwiftyTextTable-cca6ebd7
 cp -Rv SWXMLHash/.git/* ../repositories/SWXMLHash-0bda306e
+cp -Rv SWXMLHash/.git/* ../repositories/SWXMLHash-ff0954f2
 cp -Rv Yams/.git/* ../repositories/Yams-00fc82b0
+cp -Rv Yams/.git/* ../repositories/Yams-fd9f2519
 cd ../..
 patch Package.swift swiftlint.patch || exit
